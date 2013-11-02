61d9eb70ea0723f0098ed810209ffe40f7c7aded
Renato Rodrigues <hi@renatorodrigues.me>
2013-11-02 21:48:04 +0000

    Login implemented

--- BusPhoneAndroid/BusPhoneTerminal/src/main/java/edu.feup.busphone.terminal/ui/LoginActivity.java
+++ BusPhoneAndroid/BusPhoneTerminal/src/main/java/edu.feup.busphone.terminal/ui/LoginActivity.java
@@ -1,19 +1,72 @@
 package edu.feup.busphone.terminal.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import edu.feup.busphone.terminal.R;
+import edu.feup.busphone.util.network.WebServiceCallRunnable;
+import edu.feup.busphone.util.text.FormTextWatcher;
+import edu.feup.busphone.util.text.PasswordFontfaceWatcher;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements FormTextWatcher.FormListener {
+    private static final String TAG = "LoginActivity";
+
+    private EditText bus_id_edit_;
+    private EditText password_edit_;
+
+    private TextView[] required_fields_;
+
+    private Button login_button_;
 
     @Override
     protected void onCreate(Bundle saved_instance_state) {
         super.onCreate(saved_instance_state);
         setContentView(R.layout.login_activity);
+
+        bus_id_edit_ = (EditText) findViewById(R.id.bus_id_edit);
+        password_edit_ = (EditText) findViewById(R.id.password_edit);
+
+        required_fields_ = new TextView[]{bus_id_edit_, password_edit_};
+
+        login_button_ = (Button) findViewById(R.id.login_button);
+
+        PasswordFontfaceWatcher.register(password_edit_);
+        FormTextWatcher.register(required_fields_, this);
     }
 
+    public void login(View v) {
+        final String bus_id = bus_id_edit_.getText().toString();
+        final String password = password_edit_.getText().toString();
+
+        Thread login_thread = new Thread(new WebServiceCallRunnable(getWindow().getDecorView().getHandler()) {
+            @Override
+            public void run() {
+
+            }
+        });
+        login_thread.start();
+    }
+
+    @Override
+    public void updateSubmitButton() {
+        boolean enabled = true;
+
+        for (TextView field : required_fields_) {
+            if (field.getText().length() == 0) {
+                enabled = false;
+                break;
+            }
+        }
+
+        login_button_.setEnabled(enabled);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -21,6 +74,25 @@ public class LoginActivity extends Activity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_signup:
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+                finish();
+                break;
+            case R.id.action_fill:
+                fillForm();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
 
+    private void fillForm() {
+        bus_id_edit_.setText("fa6fcfc0-43ff-11e3-8f96-0800200c9a66");
+        password_edit_.setText("qwerty");
+    }
     
 }
